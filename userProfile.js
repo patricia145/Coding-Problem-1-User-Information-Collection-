78c2f4b86931ca0c20e7123bd4154e4c28499e0a
patricia145 <patriciavaldez0517@gmail.com>
2024-11-04 17:12:07 -0800

    initial commit

---
+++ userProfile.js
@@ -0,0 +1,9 @@
+let fullName = "Patricia G. Valdez";
+let age = "19";
+let favoriteNumber = "17";
+let favoriteColor = "Blue";
+
+console.log(fullName);
+console.log(age);
+console.log(favoriteNumber);
+console.log(favoriteColor);
